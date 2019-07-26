d9a83a668218ea5706b704f313cc80934268d717
Moximoxu <45614148+Moximoxu@users.noreply.github.com>
2019-07-26 16:23:14 +0800

    Index file for the main page

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>To-Do List Web App</title>
+</head>
+
+<body>
+	<?php echo '<h1 style="font-family:Times New Roman">Welcome to my To-Do List Web App!</h1>'; ?>
+</body>
+
+</html>

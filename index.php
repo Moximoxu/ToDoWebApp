935a69a83e7931eb2b1cfc8aa5a1d445a925e139
S42296 <ikhmal.zubir@gmail.com>
2019-07-29 10:08:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,63 @@
-<html>
+<!DOCTYPE html>
+<html lang="en"
 <head>
-	<title>To-Do List Web App</title>
+	<title>WhatToDo To-Do List Web App</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, intial-scale=1">
+	
+	<!--Icon-->
+	<link rel="icon" href="Resources/Sakamoto.jpg" type="image/png">
+
+	<!--Link-->
+	<link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
+	<link rel="stylesheet" 
+		href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel='stylesheet' 
+		href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+	
+	<!--Script sources-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script src="https://jqueryvalidation.org/files/lib/jquery.js"></script>
+	<script src="https://jqueryvalidation.org/files/dist/jquery.validate.js"></script>
+	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
+
+	<!--Stylesheet-->
+	<style>
+		#navLogo {
+			font-family:Georgia;
+			font-size:40px;
+		}
+	</style>
+	
+	<nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#33cc33">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link text-white" id="navLogo" href="#"><b>WhatToDo</b></a>
+			</li>
+		</ul>
+	</nav>
+	
 </head>
 
 <body>
-	<?php echo '<h1 style="font-family:Times New Roman">Welcome to my To-Do List Web App!</h1>'; ?>
+	<?php 
+	
+	?>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12"
+				<table>
+					<tr>
+						<td><input type="checkbox">Find a cat</input></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>

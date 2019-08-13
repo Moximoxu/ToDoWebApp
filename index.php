6e2fa8d853f0a685cf2ee148d43ae3295ffa2926
S42296 <ikhmal.zubir@gmail.com>
2019-08-13 17:18:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -82,6 +82,12 @@
 			margin-top; 10px;
 			box-shadow: 3px 3px 0 #ddd;
 		}
+		.tdone{
+			text-decoration:line-through;
+		}
+		.tundone{
+			text-decoration:none;
+		}
 		
 	</style>
 	
@@ -126,18 +132,22 @@
 
 				<!-- Modal Header -->
 				<div class="modal-header">
-					<h4 class="modal-title">Edit task</h4>
+					<h4 class="modal-title" id="modaltitle">Edit task</h4>
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
 				</div>
 
 				<!-- Modal body -->
 				<div class="modal-body">
 					<input type="text" class="form-control" id="editTasktxt" placeholder="Please enter a task">
+					<p id="modalmessage" style="display:none"></p>
 				</div>
 
 				<!-- Modal footer -->
 				<div class="modal-footer">
+					<button type="button" class="btn btn-info" id="btnok" style="display:none" data-dismiss="modal">Okay</button>
 					<button type="button" class="btn btn-primary save" id="btnsavechanges">Save</button>
+					<button type="button" class="btn btn-danger confirm" id="btnconfirm" style="display:none">Yes</button>
+					<button type="button" class="btn btn-info" id="btncancel" style="display:none" data-dismiss="modal">Cancel</button>
 				</div>
 
 			</div>
@@ -158,37 +168,46 @@
 				url:"fetch.php",
 				dataType: "json",
 				success: function(result){
-					var output;
+					var output, count = 1;
 					for (var i in result) {
 						if (result[i].done == 0){output +=
-							"<tr class='listoftasks' data-id='" + result[i].id + "' id='tasktr'><td class='number' id='number'><span></span>"+
-							"</td><td class='task' id='tasktxt" + result[i].id + "'>" +
-							result[i].name + "</td><td><button type='submit' class='btn btn-light done' id='btndone' data-id='" + result[i].id + "'>Done</button>"+
+							"<tr class='listoftasks' data-id='" + result[i].id + "' id='tasktr" + result[i].id + "'><td class='number' id='number'><span class='"+count+"'></span>"+
+							"</td><td class='task tundone' id='tasktxt" + result[i].id + "'>" +
+							result[i].name + 
+							"</td><td><button type='submit' class='btn btn-light done' id='btndone" + result[i].id + "' data-id='" + result[i].id + "'>Done</button>"+
+							"<button type='submit' class='btn btn-dark undone' id='btnundone" + result[i].id + "' data-id='" + result[i].id + "' style='display:none'>Undone</button>" +
 							"<button type='button' class='btn btn-success edit' data-toggle='modal' data-target='#taskModal' data-id='" + result[i].id + "'>Edit</button>" +
-							"<button type='button' class='btn btn-danger delete' id='btndelete' data-id='" + result[i].id + "'>Delete</button>"+
+							"<button type='button' class='btn btn-danger delete' id='btndelete' data-id='" + result[i].id + "' data-toggle='modal' data-target='#taskModal' >Delete</button>"+
 							"</td></tr>";
 						}	
 						else {output +=
-							"<tr class='listoftasks' data-id='" + result[i].id + "' id='tasktr'><td class='number' id='number'><span></span>" +
-							"</td><td class='task' style='text-decoration:line-through' id='tasktxt" + result[i].id + "'>" +
+							"<tr class='listoftasks' data-id='" + result[i].id + "' id='tasktr" + result[i].id + "'><td class='number' id='number'><span class='"+count+"'></span>" +
+							"</td><td class='task tdone' id='tasktxt" + result[i].id + "'>" +
 							result[i].name +
-							"</td><td><button type='submit' class='btn btn-dark undone' id='btnundone' data-id='" + result[i].id + "'>Undone</button>" +
+							"</td><td><button type='submit' class='btn btn-dark undone' id='btnundone" + result[i].id + "' data-id='" + result[i].id + "'>Undone</button>" +
+							"<button type='submit' class='btn btn-light done' id='btndone" + result[i].id + "' data-id='" + result[i].id + "' style='display:none'>Done</button>" +
 							"<button type='button' class='btn btn-success edit' data-toggle='modal' data-target='#taskModal' data-id='" + result[i].id + "'>Edit</button>" +
-							"<button type='button' class='btn btn-danger delete' id='btndelete' data-id='" + result[i].id + "'>Delete</button>"+
+							"<button type='button' class='btn btn-danger delete' id='btndelete' data-id='" + result[i].id + "' data-toggle='modal' data-target='#taskModal' >Delete</button>"+
 							"</td></tr>";
 						}
+						count++;
 					}
-				displaytasks.html(output);
-				numarrange();
-				$("table").addClass("table");
+					displaytasks.html(output);
+					numarrange(0);
+					$("table").addClass("table");
+					console.log("Tasks fetched");
 				}
 			});
 		};
 		
+		var click = 0;
 		$(document).on("keypress", "input", function(enter){
 			var check = false;
 			var code = (enter.keyCode ? enter.keyCode : enter.which);
 			if(code == 13 &! check){
+				click++;
+				var subtract = 1;
+				var num = $("#tasks_name tr").length;
 				enter.preventDefault();
 				var name = $("#addtasktxt").val();
 				console.log(name);
@@ -197,30 +216,44 @@
 				$.ajax({
 					url:"add.php",
 					type:"POST",
+					dataType:"json",
 					data: {
 						"name" : name,
 					},
-					success:function(data){
-						var html = '<tr>';
-						html += '<td>1</td>';
-						html += '<td>'+name+'</td>';
-						html += '<td><button type="submit" class="btn btn-light done" id="btndone">Done</button></td>';
+					success:function(result){
+						var id = result.id;
+						var html = '<tr class="listoftasks" data-id="' + id + '" id="tasktr' + id + '">';
+						html += '<td class="newnumber" id="newnumber"><span class="newnumber"></span></td>';
+						html += '<td class="task tundone" id="tasktxt' + id + '">' + name;
+						html += '<td><button type="submit" class="btn btn-light done" id="btndone'+id+'" data-id="'+id+'">Done</button>';
+						html += '<button type="submit" class="btn btn-dark undone" id="btnundone' + id + '" data-id="' + id + '" style="display:none">Undone</button>';
+						html += '<button type="button" class="btn btn-success edit" data-toggle="modal" data-target="#taskModal" data-id="' + id + '">Edit</button>';
+						html += '<button type="button" class="btn btn-danger delete" id="btndelete" data-id="' + id + '" data-toggle="modal" data-target="#taskModal">Delete</button>';
+						html += '</td></tr>';
+						console.log(id);
 						$('#tasks_name').prepend(html);
 						$('#add_task')[0].reset();
-						numarrange();
 					}
 				});
-				alert("Successfully added task!");
+				getalertmodal("Task added", "Successfully added task");
+				subtract += 2;
+				newnumarr(click);
+				numarrange(click);
+				console.log("Current number of click= "+click);
+				console.log("Current length of tasks name is= "+num);
 				console.log("Ajax 'add' successful!");
 			}
+			
 		});
 		
 		$(document).on("click", "button.done", function(){
 			
-			var taskname = document.getElementById("tasktr");
-			console.log(taskname);
 			var dataId = $(this).data("id");
 			console.log(dataId);
+			var taskname = $("#tasktr" +dataId);
+			console.log(taskname);
+			var bttnundone = $("#btnundone" + dataId);
+			var bttndone = $("#btndone" + dataId);
 			
 			$.ajax({
 				url:"done.php",
@@ -229,18 +262,23 @@
 					"id": dataId,
 				},
 				success:function(){
-					fetch();
+					$(taskname).toggleClass( "tdone" );
+					$(bttnundone).show("400");
+					$(bttndone).hide("400");
 				}
 				});
-				alert("Task is done!");
 				console.log("Ajax 'done' successful");
 		});
 		
 		$(document).on("click", "button.undone", function(){
 			
 			var dataId = $(this).data("id");
+			var taskname = $("#tasktr" +dataId);
+			console.log(taskname);
 			console.log(dataId);
-				
+			var bttnundone = $("#btnundone" + dataId);
+			var bttndone = $("#btndone" + dataId);
+			
 			$.ajax({
 			url:"undone.php",
 			type:"POST",
@@ -248,10 +286,11 @@
 				"id": dataId,
 			},
 			success:function(){
-				fetch();
+				$(taskname).toggleClass( "tdone" );
+				$(bttndone).show("400");
+				$(bttnundone).hide("400");
 			}
 			});
-			alert("Task is undoned!");
 			console.log("Ajax 'undone' successful");
 		});
 		
@@ -281,7 +320,7 @@
 				fetch();
 				}
 				});
-				alert("Task edited successfully!");
+				getalertmodal("Task edited", "Task successfully edited");
 				console.log("Ajax 'save' successful");
 			});
 		});
@@ -289,25 +328,34 @@
 		$(document).on("click", "button.delete", function(){
 			var dataId = $(this).data("id");
 			console.log("Current data-id is "+dataId);
+			$("#modaltitle").text("Deleting task");
+			$("#modalmessage").show("400");
+			$("#modalmessage").text("Are you sure you want to delete this task?");
+			$("#editTasktxt").hide("400");
+			$("#btnok").hide("400");
+			$("#btnconfirm").show("400");
+			$("#btncancel").show("400");
+			$("#btnsavechanges").hide("400");
 			
-			var sure = confirm("Are you sure you want to delete this task?");
-			if(sure == true){
+			$(document).on("click", "button.confirm", function(){
+			
+				console.log(dataId);
 				$.ajax({
-					url:"delete.php",
-					type:"POST",
-					data: {
-						"id" : dataId,
-					},
-					success:function(){
-					fetch();
-					}
+				url:"delete.php",
+				type:"POST",
+				data: {
+					"id" : dataId,
+				},
+				success:function(){
+				fetch();
+				}
 				});
-				alert("Task deleted successfully!");
-				console.log("Ajax 'delete' successful");
-			}
-			else{
-				alert("You cancelled!");
-			}
+				$("#btnconfirm").hide("400");
+				$("#btncancel").hide("400");
+				getalertmodal("Task deleted", "Task successfully deleted");
+				console.log("Ajax 'save' successful");
+			});
+			
 		});
 		
 		$("#add_task").on("submit", function(event){
@@ -324,27 +372,59 @@
 				},
 				success:function(data){
 					var html = '<tr>';
-					html += '<td>1</td>';
-					html += '<td>'+name+'</td>';
-					html += '<td><button type="submit" class="btn btn-light done" id="btndone">Done</button></td>';
+					html += '<td>1</td>'+
+					'<td class="task tundone" id="tasktxt"' + result[i].id + '">' + name +
+					'<td><button type="submit" class="btn btn-light done" id="btndone">Done</button></td>'+
+					'<button type="submit" class="btn btn-dark undone" id="btnundone' + result[i].id + '" data-id="' + result[i].id + '" style="display:none">Undone</button>' +
+					'<button type="button" class="btn btn-success edit" data-toggle="modal" data-target="#taskModal" data-id="' + result[i].id + '">Edit</button>' +
+					'<button type="button" class="btn btn-danger delete" id="btndelete" data-id="' + result[i].id + '" data-toggle="modal" data-target="#taskModal">Delete</button>'+
+					'</td></tr>';
 					$('#tasks_name').prepend(html);
 					$('#add_task')[0].reset();
-					numarrange();
+					numarrange(1);
 				}
 			});
-			alert("Successfully added task!");
+			getalertmodal("Task added", "Successfully added task");
 			console.log("Ajax 'add' successful!");
 		});
 		
-		function numarrange(){
+		function getalertmodal(title, message){
+			$("#modaltitle").text(title);
+			$("#modalmessage").show("400");
+			$("#modalmessage").text(message);
+			$("#editTasktxt").hide("400");
+			$("#btnok").show("400");
+			$("#btnsavechanges").hide("400");
+		}
+		
+		function numarrange(start){
 			var num = $("#tasks_name tr").length;
-			console.log(num);
-			$("td.number").each(function(index, element){
-				console.log(index);
-				console.log(element);
-				$("span").text(index);
+			var numbers = [];
+			var integ = 1;
+			//Inserting integers into array
+			for (var i=0; i < num; i++){
+				numbers [i] = integ;
+				integ++;
+			}
+			$.each(numbers, function(index, value){
+				$("span." + value).text(value+start);
 			});
 		};
+		
+		function newnumarr(start){
+			var num = $("#tasks_name tr").length;
+			var numbers = [];
+			var integ = 1;
+			//Inserting integers into array
+			for (var i=0; i < num; i++){
+				numbers [i] = integ;
+				integ++;
+			}
+			$.each(numbers, function(index, value){
+				$("span.newnumber").text(value+start);
+				console.log(value);
+			});
+		}
 		
 	});
 

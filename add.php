6a97e914797b09bf08da3d0b6a9a3ce76301de13
S42296 <ikhmal.zubir@gmail.com>
2019-08-14 16:18:51 +0800

    Update add.php

--- add.php
+++ add.php
@@ -16,5 +16,20 @@ if(isset($_POST['name'])){
 			'user' => $_SESSION['user_id']
 		]);
 	}
+	
+	$singletaskQuery = $db->prepare("
+		SELECT id, name, done
+		FROM tasks
+		WHERE created = NOW()
+	");
+	
+	// Execute the statement here
+	$singletaskQuery->execute();
+	
+	// Get data here
+	$result = $singletaskQuery->fetch();
+	
+	echo json_encode ($result);
 }
+
 ?>
